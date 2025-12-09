8ed5f31f1b3750cd905d8dcf3702b68a1f997b5e
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-09 22:48:30 +0600

    user details

---
+++ bin/map.dart
@@ -0,0 +1,10 @@
+void main(){
+  Map<String, dynamic> user={
+    'name':'Tuhin',
+    'age':24,
+    'city':'Dhaka'
+  };
+  print('User Name:${user['name']}');
+  print('User Age:${user['age']}');
+  print(user);
+}
