0b7c8926cf34f5d53c12da022981c39a8997cc46
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-10 15:39:35 +0600

    user details

--- bin/map.dart
+++ bin/map.dart
@@ -7,4 +7,17 @@ void main(){
   print('User Name:${user['name']}');
   print('User Age:${user['age']}');
   print(user);
+  print('Key:${user.keys}');
+  print('Value:${user.values}');
+  print("Entries:${user.entries}\n");
+
+  //Loop through map using forEach
+  user.forEach((key, value){
+    print('$key: $value');
+  });
+
+  //Loop using entries
+  for(var entry in user.entries){
+    print('Key:${entry.key}, Value:${entry.value}');
+  }
 }
